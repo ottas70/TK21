8273f35d07c9724988a8f8c9d4b220fff2ffb5df
Otto Vodvářka <vodvaot1@fel.cvut.cz>
2019-11-26 14:51:06 +0100

    exception Handler

--- tk21/src/main/java/cz/cvut/fel/tk21/rest/UserController.java
+++ tk21/src/main/java/cz/cvut/fel/tk21/rest/UserController.java
@@ -9,10 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
