b17aad4b50f467769ca470a2f56b107c6cd1ef55
Otto Vodvářka <vodvaot1@fel.cvut.cz>
2019-11-26 22:06:42 +0100

    Cors Filter Added

---
+++ tk21/src/main/java/cz/cvut/fel/tk21/filter/CorsFilter.java
@@ -0,0 +1,43 @@
+package cz.cvut.fel.tk21.filter;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsFilter extends OncePerRequestFilter {
+
+    @Value("${backend.allowCors}")
+    private String allowCors;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        if (this.allowsCors()) {
+            final var origin = request.getHeader("origin");
+            response.setHeader("Access-Control-Allow-Origin", origin != null ? origin : "*");
+            response.setHeader("Access-Control-Allow-Credentials", "true");
+            response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+            response.setHeader("Access-Control-Max-Age", "3600");
+            response.setHeader("Access-Control-Allow-Headers", "authorization, content-type, xsrf-token");
+            response.addHeader("Access-Control-Expose-Headers", "xsrf-token");
+        }
+        if ("OPTIONS".equals(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            filterChain.doFilter(request, response);
+        }
+    }
+
+    private boolean allowsCors() {
+        return Boolean.parseBoolean(allowCors);
+    }
+}
