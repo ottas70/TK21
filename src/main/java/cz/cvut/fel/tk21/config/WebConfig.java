b015f48cc626cfabc1bce757bc149e575acb4d33
Otto Vodvářka <vodvaot1@fel.cvut.cz>
2019-12-02 10:41:16 +0100

    Enable all origins

---
+++ src/main/java/cz/cvut/fel/tk21/config/WebConfig.java
@@ -0,0 +1,17 @@
+package cz.cvut.fel.tk21.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    //TODO change in future
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
