5a423dc1e461686f23e74b69544b51cf9a75be20
Otto Vodvářka <vodvaot1@fel.cvut.cz>
2019-12-02 10:31:13 +0100

    Club retrieval

--- src/main/java/cz/cvut/fel/tk21/rest/ClubController.java
+++ src/main/java/cz/cvut/fel/tk21/rest/ClubController.java
@@ -1,5 +1,8 @@
 package cz.cvut.fel.tk21.rest;
 
+import cz.cvut.fel.tk21.exception.NotFoundException;
+import cz.cvut.fel.tk21.model.Club;
+import cz.cvut.fel.tk21.rest.dto.ClubDto;
 import cz.cvut.fel.tk21.rest.dto.ClubRegistrationDto;
 import cz.cvut.fel.tk21.service.ClubService;
 import cz.cvut.fel.tk21.util.RequestBodyValidator;
@@ -9,12 +12,10 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/club")
@@ -32,6 +33,13 @@ public class ClubController {
         Integer clubId = clubService.registerClub(club);
         final HttpHeaders headers = RestUtils.createLocationHeaderFromCurrentUri("/{id}", clubId);
         return new ResponseEntity<>(headers, HttpStatus.CREATED);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ClubDto getClub(@PathVariable("id") Integer id) {
+        final Optional<Club> club = clubService.find(id);
+        club.orElseThrow(() -> new NotFoundException("Klub nebyl nalezen"));
+        return new ClubDto(club.get());
     }
 
 }
