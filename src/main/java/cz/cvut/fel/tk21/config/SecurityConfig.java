213a84e36ca206129433609d849e2e29edcfb8bc
Otto Vodvářka <vodvaot1@fel.cvut.cz>
2019-12-03 22:04:27 +0100

    Security update

--- src/main/java/cz/cvut/fel/tk21/config/SecurityConfig.java
+++ src/main/java/cz/cvut/fel/tk21/config/SecurityConfig.java
@@ -45,8 +45,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
 
-        http.authorizeRequests().antMatchers("/", "/static/**", "/api/authenticate", "/api/user",
-                "/api/logout", "/api/confirm").permitAll();
+        http.authorizeRequests().antMatchers("/*", "/static/**",
+                "/api/authenticate", "/api/user", "/api/logout", "/api/confirm")
+                .permitAll();
 
         http.authorizeRequests().anyRequest().authenticated();
 
