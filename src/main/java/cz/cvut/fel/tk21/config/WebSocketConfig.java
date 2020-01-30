0bb528f9d5d63a55f63f7d558aaa49a6af970bf4
Otto Vodvářka <vodvaot1@fel.cvut.cz>
2020-01-30 23:13:53 +0100

    Trial

--- src/main/java/cz/cvut/fel/tk21/config/WebSocketConfig.java
+++ src/main/java/cz/cvut/fel/tk21/config/WebSocketConfig.java
@@ -2,18 +2,32 @@ package cz.cvut.fel.tk21.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.http.server.ServletServerHttpRequest;
-import org.springframework.http.server.reactive.ServerHttpRequest;
-import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.ChannelInterceptorAdapter;
+import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.server.HandshakeHandler;
+import org.springframework.web.socket.server.HandshakeInterceptor;
 import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
+import org.springframework.web.util.WebUtils;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.security.Principal;
+import java.util.List;
 import java.util.Map;
 
 @Configuration
@@ -29,22 +43,55 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         //TODO remove origin
         registry.addEndpoint("/websocket")
-                .setHandshakeHandler(handshakeHandler())
                 .setAllowedOrigins("*")
-                .withSockJS();
+                .withSockJS()
+                .setInterceptors(httpSessionHandshakeInterceptor());
     }
 
     @Bean
-    public HandshakeHandler handshakeHandler(){
-        return new DefaultHandshakeHandler(){
-            public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map attributes) throws Exception {
-                if (request instanceof ServletServerHttpRequest) {
-                    ServletServerHttpRequest servletRequest = (ServletServerHttpRequest) request;
-                    HttpSession session = servletRequest.getServletRequest().getSession();
-                    attributes.put("sessionId", session.getId());
+    public HandshakeInterceptor httpSessionHandshakeInterceptor() {
+        return new HandshakeInterceptor() {
+
+            @Override
+            public boolean beforeHandshake(ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse, WebSocketHandler webSocketHandler, Map<String, Object> map) throws Exception {
+                if (serverHttpRequest instanceof ServletServerHttpRequest) {
+                    ServletServerHttpRequest servletServerRequest = (ServletServerHttpRequest) serverHttpRequest;
+                    HttpServletRequest servletRequest = servletServerRequest.getServletRequest();
+                    Cookie token = WebUtils.getCookie(servletRequest, "Credentials");
+                    if (token == null) {
+                        map.put("token", "null");
+                    } else {
+                        map.put("token", token.getValue());
+                    }
                 }
                 return true;
             }
+
+            @Override
+            public void afterHandshake(ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse, WebSocketHandler webSocketHandler, Exception e) {
+
+            }
         };
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(new ChannelInterceptor() {
+            @Override
+            public Message<?> preSend(Message<?> message, MessageChannel channel) {
+                StompHeaderAccessor accessor =
+                        MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+                if (StompCommand.CONNECT.equals(accessor.getCommand())) {
+                    Map<String, Object> sessionAttributes = accessor.getSessionAttributes();
+                    String token = (String) sessionAttributes.get("token");
+                    if(token != null){
+                        //Principal user = ... ;
+                        //accessor.setUser();
+                    }
+                }
+
+                return message;
+            }
+        });
     }
 }
--- src/main/java/cz/cvut/fel/tk21/ws/WebSocketController.java
+++ src/main/java/cz/cvut/fel/tk21/ws/WebSocketController.java
@@ -4,9 +4,11 @@ import cz.cvut.fel.tk21.exception.NotFoundException;
 import cz.cvut.fel.tk21.exception.ValidationException;
 import cz.cvut.fel.tk21.model.Club;
 import cz.cvut.fel.tk21.model.Reservation;
+import cz.cvut.fel.tk21.model.User;
 import cz.cvut.fel.tk21.rest.dto.reservation.CreateReservationDto;
 import cz.cvut.fel.tk21.service.ClubService;
 import cz.cvut.fel.tk21.service.ReservationService;
+import cz.cvut.fel.tk21.service.UserService;
 import cz.cvut.fel.tk21.ws.dto.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -28,11 +30,16 @@ public class WebSocketController {
     @Autowired
     private ClubService clubService;
 
+    @Autowired
+    private UserService userService;
+
 
     //Kdyz neprijde datum, tak se najde nejbližší možná rezervace
     @MessageMapping("/ws/reservation/{clubId}")
     @SendToUser("/topic/reservation/{clubId}")
     public ReservationMessage initialMessage(@DestinationVariable Integer clubId, @Payload(required = false) DateDto date, Principal principal){
+        //User user = userService.getCurrentUser();
+
         Optional<Club> club = clubService.find(clubId);
         club.orElseThrow(() -> new NotFoundException("Klub nebyl nalezen"));
 
