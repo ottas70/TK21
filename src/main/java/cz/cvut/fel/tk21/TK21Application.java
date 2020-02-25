0ab2a00052c6e63c8d09286fa58c3d2514b9cb1f
Otto Vodvářka <vodvaot1@fel.cvut.cz>
2020-02-25 11:52:18 +0100

    Set timezone to CET

--- src/main/java/cz/cvut/fel/tk21/TK21Application.java
+++ src/main/java/cz/cvut/fel/tk21/TK21Application.java
@@ -4,9 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableScheduling
 public class TK21Application {
+
+    @PostConstruct
+    public void init(){
+        TimeZone.setDefault(TimeZone.getTimeZone("CET"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(TK21Application.class, args);
